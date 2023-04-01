de397f2ed54fd0cc5d033e7165be44e4ccc3afbf
httpcyberdata <shieldedsafety@gmail.com>
2023-04-01 19:24:51 -0400

    chore: created /checkout POST route for handling Stripe payments

--- server/server.js
+++ server/server.js
@@ -7,4 +7,33 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
-app.post('/')
+app.post('/checkout', async (req, res) => {
+	console.log(req.body);
+	const items = req.body.items;
+	let lineItems = [];
+
+	items.forEach((item) => {
+		lineItems.push(
+			{
+				price: item.id,
+				quantity: item.quantity
+			}
+		)
+	})
+
+
+	const session = await stripe.checkout.sessions.create({
+		line_items: lineItem,
+		mode: 'payment',
+		success_url: 'http://localhost:3000/success',
+		cancel_url: 'http://localhost:3000/cancel'
+
+	});
+
+	res.send(JSON.stringify({
+		url: session.url
+	}));
+
+});
+
+app.listen(4000, () => console.log("Listening on port 4000"))
